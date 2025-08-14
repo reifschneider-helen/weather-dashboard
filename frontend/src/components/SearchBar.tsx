3406c2dec9f0d1ca490c962dc3fc56d0043e3ea6
reifschneider-helen <reifschneider.helen@gmail.com>
2025-08-14 13:10:44 +0200

    fix: Correct input ID in SearchBar component for better accessibility

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -24,7 +24,7 @@ export default function SearchBar(props: {
 
   return (
     <input
-      id="search"
+      id="weather-location-input"
       className="bg-white dark:bg-gray-800 text-black dark:text-gray-100 placeholder:text-gray-500 dark:placeholder:text-gray-400 shadow-md outline-none w-full px-4 py-3 rounded-lg"
       placeholder="Ort eingeben..."
       type="text"
