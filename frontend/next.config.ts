8b72a3f7bb1b2d9fcc76758bcd35d08acb012e46
reifschneider-helen <reifschneider.helen@gmail.com>
2025-10-03 14:45:02 +0200

    fix: Update content security policy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,41 @@ const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src *; script-src 'self' 'unsafe-inline' 'unsafe-eval'  https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src * data:; connect-src *; font-src *;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
